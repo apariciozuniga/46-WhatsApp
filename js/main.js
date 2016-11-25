b94ed8ce083f7c4004ef284393dbfe8d27bfde41
apariciozuniga <bereniceaparicio31@gmail.com>
2016-11-25 16:55:36 +0000

    subiendo mi primer trabajo en materialize y falta poner efectos y scroll

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// codigo javascript	
+
+// codigo para mis botones
+
+  $('.fixed-action-btn').openFAB();
+  $('.fixed-action-btn').closeFAB();
+  $('.fixed-action-btn.toolbar').openToolbar();
+  $('.fixed-action-btn.toolbar').closeToolbar();
